1ff2a5994a1f1a865ac6afe692f328dad119ace3
Max Sharov <maxsharoff@gmail.com>
2018-09-05 15:18:58 +0300

    Change app script endpoints for development

--- src/index.js
+++ src/index.js
@@ -1,7 +1,1 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
-import registerServiceWorker from './services/registerServiceWorker'
-
-ReactDOM.render(<App />, document.getElementById('root'))
-registerServiceWorker()
+export { Button } from "components";
