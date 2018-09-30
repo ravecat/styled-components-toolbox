db70d4303be9243aefd4bf7f8464e3b66343367a
Max Sharov <maxsharoff@gmail.com>
2018-09-30 23:02:40 +0300

    Add helper for set default story

---
+++ src/common/helpers/index.ts
@@ -0,0 +1,2 @@
+export { default as withTheme } from "./withTheme";
+export { default as setDefaultStory } from "./setDefaultStory";
--- src/components/Header/Header.stories.js
+++ src/components/Header/Header.stories.js
@@ -1,22 +1,20 @@
 import * as React from "react";
-import { storiesOf } from "@storybook/react";
 import { text, boolean, select } from "@storybook/addon-knobs";
-import { withReadme } from "storybook-readme";
+import { setDefaultStory } from "common/helpers";
 import Header from "./index";
 import * as readme from "./README.md";
 
 const DEFAULT_TAG = "h1";
 const OPTIONS = ["h1", "h2", "h3", "h4", "h5", "h6"];
 
-storiesOf("Header", module).add(
-  "Default",
-  withReadme(readme, () => (
-    <Header
-      tag={select("tag", OPTIONS, DEFAULT_TAG)}
-      uppercase={boolean("uppecase", false)}
-      bold={boolean("bold", false)}
-    >
-      {text("value", "Header")}
-    </Header>
-  ))
+setDefaultStory(
+  "Header",
+  readme,
+  <Header
+    tag={select("tag", OPTIONS, DEFAULT_TAG)}
+    uppercase={boolean("uppecase", false)}
+    bold={boolean("bold", false)}
+  >
+    {text("value", "Header")}
+  </Header>
 );
