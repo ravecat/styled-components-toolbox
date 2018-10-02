5263bb56680d4897760d5ac69bb415f73cc3698f
Max Sharov <maxsharoff@gmail.com>
2018-10-02 23:36:50 +0300

    Update stories

--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -1,20 +1,16 @@
 import * as React from "react";
-import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
 import { text, boolean } from "@storybook/addon-knobs";
-import { withReadme } from "storybook-readme";
+import { setDefaultStory } from "common/helpers";
 import Button from "./index";
 import * as readme from "./README.md";
 
-storiesOf("Button", module).add(
-  "Default",
-  withReadme(readme, () => (
-    <Button
-      disabled={boolean("disabled", false)}
-      href={text("href")}
-      onClick={action("clicked")}
-    >
-      {text("value", "Button")}
-    </Button>
-  ))
-);
+setDefaultStory("Button", readme, () => (
+  <Button
+    disabled={boolean("disabled", false)}
+    href={text("href")}
+    onClick={action("clicked")}
+  >
+    {text("value", "Button")}
+  </Button>
+));
--- src/components/Icon/Icon.stories.js
+++ src/components/Icon/Icon.stories.js
@@ -1,7 +1,6 @@
 import * as React from "react";
-import { storiesOf } from "@storybook/react";
 import { text, boolean, number } from "@storybook/addon-knobs";
-import { withReadme } from "storybook-readme";
+import { setDefaultStory } from "common/helpers";
 import Icon from "./index";
 import * as readme from "./README.md";
 
@@ -11,17 +10,14 @@ const ALTERNATIVE =
   "https://cdn2.iconfinder.com/data/icons/ios-7-icons/50/mute-512.png";
 const sizeOptions = { range: true, min: 30, max: 120, step: 5 };
 
-storiesOf("Icon", module).add(
-  "Default",
-  withReadme(readme, () => (
-    <Icon
-      alt={text("alt")}
-      alternative={text("alternative", ALTERNATIVE)}
-      asset={text("asset", ASSET)}
-      condition={boolean("condition", false)}
-      fill={boolean("fill", false)}
-      height={number("height", 30, sizeOptions)}
-      width={number("width", 30, sizeOptions)}
-    />
-  ))
-);
+setDefaultStory("Icon", readme, () => (
+  <Icon
+    alt={text("alt")}
+    alternative={text("alternative", ALTERNATIVE)}
+    asset={text("asset", ASSET)}
+    condition={boolean("condition", false)}
+    fill={boolean("fill", false)}
+    height={number("height", 30, sizeOptions)}
+    width={number("width", 30, sizeOptions)}
+  />
+));
