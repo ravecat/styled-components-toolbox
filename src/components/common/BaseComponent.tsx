5ef2d272611fbc80be7a09aa4431a34451f904b9
Max Sharov <maxsharoff@gmail.com>
2018-10-08 00:14:31 +0300

    Add BaseComponent

---
+++ src/components/common/BaseComponent.tsx
@@ -0,0 +1,21 @@
+import { ReactNode } from "react";
+import { ThemeInterface } from "common/theme";
+import styled, { theme } from "common/theme";
+
+export interface BaseProps {
+  theme: ThemeInterface;
+  children?: ReactNode;
+  as?: string;
+}
+
+const BaseComponent = styled.div<BaseProps>`
+  margin: 0;
+  padding: 0;
+  box-sizing: ${theme.boxSizing};
+  font-family: ${theme.fontFamily};
+  font-size: ${theme.fontSize};
+  font-weight: ${theme.fontWeight};
+  color: ${theme.primaryTextColor};
+`;
+
+export default BaseComponent;
