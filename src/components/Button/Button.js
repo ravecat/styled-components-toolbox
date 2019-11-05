be9728b738c95df55372dfd86a907d864f8ea535
pro-frontend <dzmitryj.ivchenkov@gmail.com>
2019-11-05 15:32:20 +0300

    feat(components): add block type for component Button
    META: #78

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -30,6 +30,7 @@ const Wrapper = styled(BaseComponent).attrs(({ type = "button" }) => ({
     background: ${theme.textColor};
     color: ${theme.primaryColor};
   }
+<<<<<<< HEAD
 
   ${({ disabled }) =>
     disabled &&
@@ -110,6 +111,68 @@ const Wrapper = styled(BaseComponent).attrs(({ type = "button" }) => ({
         background: rgba(0, 0, 0, 0);
       }
     `};
+=======
+  
+  ${({ disabled }) => disabled && css`
+    && {
+      cursor: default;
+      background: ${theme.textColor};
+      border-color: ${theme.dividerColor};
+      color: ${theme.dividerColor};
+    }
+  `};
+  
+  ${({ primary }) => primary && css`
+    && {
+      background: ${theme.secondaryTextColor};
+    }
+    
+    &:hover {
+      background: ${theme.primaryTextColor};
+      color: ${theme.textColor};
+    }
+  `};
+  
+  ${({ dashed }) => dashed && css`
+    && {
+      background: ${theme.textColor};
+      border-color: ${theme.dividerColor};
+      border: 1px dashed ${theme.lightPrimaryColor};
+      color: ${theme.dividerColor};
+    }
+    
+    &:hover {
+      border-color: ${theme.accentColor};
+      color: ${theme.accentColor};
+    }
+  `};
+  
+  ${({ rounded }) => rounded && css`
+    && {
+      border-radius: 4px;
+    }
+  `};
+  
+  ${({ block }) => block && css`
+    && {
+      display: block;
+      width: 100%;
+    }
+  `};
+  
+  ${({ link }) => link && css`
+    && {
+      background: rgba(0, 0, 0, 0);
+      border: 0;
+      color: ${theme.primaryColor};
+      text-decoration: underline;
+    }
+    
+    &:hover {
+      color: ${theme.accentColor};
+    }
+  `};
+>>>>>>> feat(components): add block type for component Button
 `;
 
 export default Button;
--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -4,6 +4,7 @@ import { action } from "@storybook/addon-actions";
 import { text, boolean } from "@storybook/addon-knobs";
 import Button from "./index";
 
+<<<<<<< HEAD
 storiesOf("Button", module).add("Default", () => (
   <Button
     block={boolean("block", false)}
@@ -19,3 +20,20 @@ storiesOf("Button", module).add("Default", () => (
     {text("value", "Button")}
   </Button>
 ));
+=======
+storiesOf("Button", module)
+  .add("Default", () => (
+    <Button
+      block={boolean("block", false)}
+      dashed={boolean("dashed", false)}
+      disabled={boolean("disabled", false)}
+      href={text("href")}
+      link={boolean("link", false)}
+      onClick={action("clicked")}
+      primary={boolean("primary", false)}
+      rounded={boolean("rounded", false)}
+    >
+      {text("value", "Button")}
+    </Button>
+  ));
+>>>>>>> feat(components): add block type for component Button
